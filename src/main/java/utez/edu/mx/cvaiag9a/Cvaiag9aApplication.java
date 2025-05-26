2d5d9f21da8acc930c9a2ee215e0edce50d9420d
BojitaNoir <20213tn094@utez.edu.mx>
2025-05-26 17:44:06 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cvaiag9a/Cvaiag9aApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvaiag9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvaiag9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvaiag9aApplication.class, args);
+    }
+
+}
